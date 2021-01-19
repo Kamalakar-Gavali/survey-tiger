8529ad051c1cbf459cdb9432455572e407eba98d
Kamalakar-Gavali <43421458+Kamalakar-Gavali@users.noreply.github.com>
2021-01-19 19:46:31 +0530

    adddd all files

---
+++ src/Store/global-store.js
@@ -0,0 +1,5 @@
+import {configureStore,createSlice,combineReducers} from '@reduxjs/toolkit'
+import {surveySlice} from './surveySlice';
+import responseSlice from './responseSlice'
+const rootReducer=combineReducers({surveys:surveySlice.reducer,responses:responseSlice.reducer});
+export const store=configureStore({reducer:rootReducer});
